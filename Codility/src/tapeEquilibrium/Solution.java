bb64ea08014f9c98f965ce7c1efe7574f6240586
ymd2357 <ymd2357@github.com>
2020-06-06 03:21:57 +0900

    パフォーマンス改善。Detected time complexity:O(N)

--- Codility/src/tapeEquilibrium/Solution.java
+++ Codility/src/tapeEquilibrium/Solution.java
@@ -3,22 +3,23 @@ package tapeEquilibrium;
 /**
  * Lesson3:Time Complexity: TapeEquilibrium
  *
+ *
+ * 2 <= N <= 100,000
+ * -1,000 <= A <= 1,000
+ *
  * @see https://app.codility.com/programmers/lessons/3-time_complexity/tape_equilibrium/
  */
 class Solution {
 	public int solution(int[] A) {
+		int N = A.length;
 		int temp = Integer.MAX_VALUE;
-		for (int P = 1; P < A.length; P++) {
-			int sum1 = 0;
-			int sum2 = 0;
-			for (int i = 0; i < A.length; i++) {
-				if (i < P) {
-					sum1 += A[i];
-				} else {
-					sum2 += A[i];
-				}
-			}
-			int abs = Math.abs(sum1 - sum2);
+		int total = 0;
+		for (int P = 0; P < N; P++) {
+			total += A[P];
+		}
+		for (int P = 1; P < N; P++) {
+			total -= A[P-1] * 2;
+			int abs = Math.abs(total);
 //			System.out.println("P = " + P + " : " + abs);
 
 			temp = Math.min(temp, abs);
